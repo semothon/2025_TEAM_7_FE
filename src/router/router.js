b8a483700a8824414ffd0bb59c8d12eb7d935c1e
woooo <99981511+xhae000@users.noreply.github.com>
2025-04-01 00:18:43 +0900

    모임생성 페이지 router 설정

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import WhatParty from '../views/CreateParty/WhatParty.vue'
+import HowApply from '../views/CreateParty/HowApply.vue'
+import PartyIntro1 from '../views/CreateParty/PartyIntro1.vue'
+import PartyIntro2 from '../views/CreateParty/PartyIntro2.vue'
+import WhatPeople from '../views/CreateParty/WhatPeople.vue'
+import RegisterImage from '../views/CreateParty/RegisterImage.vue'
+
 
 const routes = [
+
   {
     path: '/home',
     name: 'Home',
     component: HomeView
+  },
+
+  {
+    path: '/create-party/what-party',
+    component: WhatParty
+  },
+  
+  {
+    path: '/create-party/how-apply',
+    component: HowApply
+  },
+  
+  {
+    path: '/create-party/party-intro-1',
+    component: PartyIntro1
+  },
+
+  {
+    path: '/create-party/party-intro-2',
+    component: PartyIntro2
+  },
+  
+  {
+    path: '/create-party/what-people',
+    component: WhatPeople
+  },
+  
+  {
+    path: '/create-party/register-image',
+    component: RegisterImage
   },
   
 ]
